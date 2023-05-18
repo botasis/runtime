bd96b2c556bcbef3c892916264ceda91d37dddff
viktorprogger <viktorprogger@gmail.com>
2023-05-18 14:53:15 +0600

    Add route groups

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace Botasis\Runtime;
+namespace Botasis\Runtime\Router;
 
 use Botasis\Runtime\Update\Update;
+use Botasis\Runtime\UpdateHandlerInterface;
 use InvalidArgumentException;
 use Psr\Container\ContainerInterface;
 
@@ -11,6 +12,7 @@ final readonly class Router
     public const ROUTE_KEY_RULE_STATIC = 'rule_static';
     public const ROUTE_KEY_RULE = 'rule';
     public const ROUTE_KEY_ACTION = 'action';
+    public const ROUTE_KEY_ACTION_LIST = 'actions';
     private array $rulesStatic;
 
     /**
@@ -19,10 +21,19 @@ final readonly class Router
     public function __construct(private ContainerInterface $container, private array $routes)
     {
         $rulesStatic = [];
-        foreach ($this->routes as $route) {
+        foreach ($this->routes as &$route) {
+            $isGroup = ($route[self::ROUTE_KEY_ACTION_LIST] ?? []) !== [];
+            if (!$isGroup xor is_string($route[self::ROUTE_KEY_ACTION])) {
+                throw new InvalidArgumentException('Telegram route must have either "action" or "actions" key.');
+            }
+
+            if ($isGroup) {
+                $route[self::ROUTE_KEY_ACTION] = new self($this->container, $route[self::ROUTE_KEY_ACTION_LIST]);
+            }
+
             $staticRule = $this->getRuleStatic($route);
             $hasStaticRule = $staticRule !== [];
-            if (!$hasStaticRule xor isset($route['rule'])) {
+            if (!$hasStaticRule xor isset($route[self::ROUTE_KEY_RULE])) {
                 throw new InvalidArgumentException('Telegram route must have either "rule_static" or "rule" key.');
             }
 
@@ -30,23 +41,37 @@ final readonly class Router
                 $rulesStatic[$staticRule[0]] = $staticRule[1];
             }
         }
+        unset($route);
 
         $this->rulesStatic = $rulesStatic;
     }
 
     public function match(Update $update): UpdateHandlerInterface
     {
+        $action = null;
         if (isset($this->rulesStatic[$update->requestData])) {
-            return $this->container->get($this->rulesStatic[$update->requestData]); // TODO validate action
+            $action = $this->rulesStatic[$update->requestData];
         }
 
         foreach ($this->routes as $route) {
-            if ($route['rule']($update)) {
-                return $this->container->get($route['action']); // TODO validate action
+            if ($route[self::ROUTE_KEY_RULE]($update)) {
+                $action = $route[self::ROUTE_KEY_ACTION];
             }
         }
 
-        throw new NotFoundException($update);
+        if (is_string($action)) {
+            return $this->container->get($action);
+        }
+
+        if ($action instanceof self) {
+            return $action->match($update);
+        }
+
+        if ($action === null) {
+            throw new NotFoundException($update);
+        }
+
+        throw new InvalidArgumentException('Telegram route action must be a string identifier of an UpdateHandlerInterface class.');
     }
 
     private function getRuleStatic(array $route): array
