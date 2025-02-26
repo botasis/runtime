9033be93b2c52cbe04e7adc1c738e2989c686d5b
Viktor Babanov <viktorprogger@gmail.com>
2025-02-26 15:24:00 +0500

    Replace composer require checker with composer dependency analyser (#29)
    * Replace composer require checker with composer dependency analyser
    
    * bugfix
    
    * Rename CI step

--- tests/Middleware/MiddlewareFactoryTest.php
+++ tests/Middleware/MiddlewareFactoryTest.php
@@ -12,7 +12,6 @@ use Botasis\Runtime\Middleware\Exception\InvalidMiddlewareDefinitionException;
 use Botasis\Runtime\Middleware\MiddlewareFactory;
 use Botasis\Runtime\Middleware\MiddlewareFactoryInterface;
 use Botasis\Runtime\Middleware\MiddlewareInterface;
-use Botasis\Runtime\Request\TelegramRequestEnriched;
 use Botasis\Runtime\Response\Response;
 use Botasis\Runtime\Response\ResponseInterface;
 use Botasis\Runtime\Tests\Middleware\Support\InvalidController;
